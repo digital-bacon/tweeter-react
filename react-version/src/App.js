8088ebf388062281cc8098a70c8154fa0039d63d
Mark Bieganek <mark.bieganek@telus.com>
2023-01-07 13:57:17 -0700

    Move static assets into public/, update class to className

--- react-version/src/App.js
+++ react-version/src/App.js
@@ -9,8 +9,10 @@ function App() {
     <div className="App">
       <Navigation />
       <Profile />
-      <TweetList />
+      <main className="container">
       <TweetForm />
+      <TweetList />
+      </main>
     </div>
   );
 }
