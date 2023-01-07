89c2e101c3244de34edca04ab5e7dee7b7136415
Mark Bieganek <mark.bieganek@telus.com>
2023-01-07 14:53:25 -0700

    Prevent default behaviour on form submit

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -1,9 +1,13 @@
 function TweetForm() {
   const question = "What are you humming about?";
+  const doSubmit = (event) => {
+    event.preventDefault();
+    console.log(`The request body contains: ${ event.target.text.value }`);
+  };
   
   return (
     <section className="newtweet">
-      <form method="post" action="/tweets" className="newtweet__form">
+      <form method="post" action="/tweets" className="newtweet__form" onSubmit={ doSubmit }>
         <textarea className="form__textarea" name="text" placeholder={ question }></textarea>
         <input type="submit" value="Tweet" className="form__input" />
         <span className="form__counter">140</span>
