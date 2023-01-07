fa0036184c87f0fe990f92e85fb9236032e1e8e5
Mark Bieganek <mark.bieganek@telus.com>
2023-01-07 13:25:10 -0700

    Import components into App.js, call components in App()

--- react-version/src/App.js
+++ react-version/src/App.js
@@ -1,8 +1,17 @@
+import Navigation from './components/Navigation';
+import Profile from './components/Profile';
+import TweetList from './components/TweetList';
+import TweetForm from './components/TweetForm';
 import './App.css';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Navigation />
+      <Profile />
+      <TweetList />
+      <TweetForm />
+    </div>
   );
 }
 
